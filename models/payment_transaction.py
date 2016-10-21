33f3477e6036f4ebb8562047198a6c7fea5d7cf5
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2016-10-21 12:46:17 +0200

    [IMP] Add scheduling errors management

--- models/payment_transaction.py
+++ models/payment_transaction.py
@@ -22,7 +22,7 @@
 #
 ##############################################################################
 
-from openerp import models
+from openerp import models, api
 
 
 class PaymentTransactionOgone(models.Model):
@@ -30,6 +30,32 @@ class PaymentTransactionOgone(models.Model):
 
     # ogone status
     _ogone_valid_tx_status = [5, 9, 56]
+    _ogone_error_tx_status = {
+        57: 'Not OK with scheduled payments',
+        6: 'Authorised and cancelled',
+    }
 
+    @api.model
+    def _ogone_form_validate(self, tx, data):
+        status = int(data.get('STATUS') or 0)
+        if status in self._ogone_error_tx_status:
+            tx.write({
+                'state': 'error',
+                'state_message': self._ogone_error_tx_status[status],
+            })
+            return True
+
+        return super(PaymentTransactionOgone, self)._ogone_form_validate(tx, data)
+
+    @api.model
+    def _ogone_form_get_invalid_parameters(self, tx, data):
+        invalid_parameters = super(PaymentTransactionOgone, self)._ogone_form_get_invalid_parameters(tx, data)
+
+        if len(tx.payment_ids) > 1:
+            for parameter in invalid_parameters:
+                if parameter[0] == 'amount':
+                    invalid_parameters.remove(parameter)
+
+        return invalid_parameters
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
